e383867452bc44b8f9b096a13a54c60606fc4475
Andre Kovac <me@andrekovac.com>
2016-06-18 17:46:36 +0200

    Remove hug back-end and implement Flask back-end because hug is not compatible with python2.7, only python3.+

--- src/handlers/submit.py
+++ src/handlers/submit.py
@@ -4,16 +4,18 @@ import json
 from hug.types import longer_than, text
 from ..database.db import db
 
-# image should come as a base64 string, which will be a certain length
-@hug.post("/submit")
-def submit(image: longer_than(100, convert=text)):
-  # image will be a variable with base64 string
-  
-  # takes the image from a json file as base64
-
-  # perform your image manipulation here, transform to json coords
-
-  # cut out actual images from coordinates
-
-  # return images as an array of base64 strings
-  return json.dumps({ "bubu": "hello" })
+# from ..processor.crop import getImagesAsJson
+#
+# # image should come as a base64 string, which will be a certain length
+# @hug.post("/submit")
+# def submit(image: longer_than(100, convert=text)):
+#   # image will be a variable with base64 string
+#
+#   # takes the image from a json file as base64
+#
+#   # perform your image manipulation here, transform to json coords
+#
+#   # cut out actual images from coordinates
+#
+#   # return images as an array of base64 strings
+#   return json.dumps({ "bubu": image })
